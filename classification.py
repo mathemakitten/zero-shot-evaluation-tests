428c502cfda368cde009a2521d0c7413570eb210
helen <helen@huggingface.co>
2022-07-27 19:55:04 -0700

    distributed dataset, also fix the parsing from csv

--- classification.py
+++ classification.py
@@ -2,6 +2,7 @@
 
 import os
 
+import ast
 import time
 import deepspeed
 import pandas as pd
@@ -11,27 +12,77 @@ import torch.nn.functional as F
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 from transformers.deepspeed import HfDeepSpeedConfig
 
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data import DataLoader
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"  # To avoid warnings about parallelism in tokenizers
+
+# distributed setup
+local_rank = int(os.getenv('LOCAL_RANK', '0'))
+world_size = int(os.getenv('WORLD_SIZE', '1'))
+torch.cuda.set_device(local_rank)
+deepspeed.init_distributed()
+
+
+def get_logits_and_tokens(self, prompts):
+    all_logits = []
+    all_tokens = []
+
+    for prompt in prompts:
+        tokenized_inputs = self.tokenizer(
+            prompt, return_tensors="pt", truncation=True
+        ).to(device=local_rank)
+        outputs = self.ds_engine.module(**tokenized_inputs)
+        logits = outputs["logits"].detach().to(device="cpu", dtype=torch.float32)
+
+        # need to remove batch dimension
+        all_logits.append(torch.squeeze(logits))
+        all_tokens.append(torch.squeeze(tokenized_inputs["input_ids"]))
+    return all_logits, all_tokens
+
+
+def load_dataset(num_truncate=None):
+    df = pd.read_csv('conjunction_fallacy-0shot.csv').iloc[:, 1:]  # drop nonsensical 1st column
+    if num_truncate:
+        df = df[:num_truncate]
+        print(f"Running with dataset truncated to {num_truncate} examples")
+    examples = df.to_dict('records')
+
+    prompts = [
+        example["prompt"] + class_seq
+        for example in examples
+        for class_seq in ast.literal_eval(example["classes"])
+    ]
+    return prompts
+
+
+def prepare(dataset, batch_size=8, pin_memory=False, num_workers=0):
+    sampler = DistributedSampler(dataset, num_replicas=world_size,
+                                 rank=torch.distributed.get_rank(),
+                                 shuffle=False,
+                                 drop_last=False)
+
+    dataloader = DataLoader(dataset, batch_size=batch_size, pin_memory=pin_memory, num_workers=num_workers,
+                            drop_last=False, shuffle=False, sampler=sampler)
+
+    return dataloader
+
 
 class Inference:
 
     def __init__(self):
-        os.environ["TOKENIZERS_PARALLELISM"] = "false"  # To avoid warnings about parallelism in tokenizers
 
-        # distributed setup
-        self.local_rank = int(os.getenv('LOCAL_RANK', '0'))
-        self.world_size = int(os.getenv('WORLD_SIZE', '1'))
-        torch.cuda.set_device(self.local_rank)
-        deepspeed.init_distributed()
+        self.model_name = 'facebook/opt-6.7b'
+        # self.model_name = "facebook/opt-13b"
 
-        # model_name = 'facebook/opt-6.7b'
-        self.model_name = "facebook/opt-13b"
         # FYI: from_pretrained(..., low_cpu_mem_usage=True) is incompatible with zero.init stage 3
         #  normally Transformers uses 2x of model size in CPU memory while loading the model
         self.config = AutoConfig.from_pretrained(self.model_name)
         # print(config)
         self.model_hidden_size = self.config.hidden_size
 
-        self.train_batch_size = 1 * self.world_size
+        self.train_batch_size = 1 * world_size
 
         ds_config = {
             "fp16": {
@@ -73,95 +124,48 @@ class Inference:
                                               lr_scheduler=None)[0]
         self.ds_engine.module.eval()  # inference
 
-    def load_dataset(self, num_truncate=None):
-        df = pd.read_csv('conjunction_fallacy-0shot.csv').iloc[:, 1:]  # drop nonsensical 1st column
-        if num_truncate:
-            df = df[:num_truncate]
-            print(f"Running with dataset truncated to {num_truncate} examples")
-        examples = df.to_dict('records')
-        return examples
+    def evaluate_classification(self, distributed_dataset):
 
-    def get_logits_and_tokens(self, prompts):
         all_logits = []
         all_tokens = []
 
-        for prompt in prompts:
-            tokenized_inputs = self.tokenizer(
-                prompt, return_tensors="pt", truncation=True
-            ).to(device=self.local_rank)
-            outputs = self.ds_engine.module(**tokenized_inputs)
-            logits = outputs["logits"].detach().to(device="cpu", dtype=torch.float32)
+        while True:
+            try:
+                batch = next(distributed_dataset)
+                for x in batch:  # TODO: this should technically be able to batch
+                    tokenized_inputs = self.tokenizer(x, return_tensors="pt", truncation=True).to(device=local_rank)
+                    outputs = self.ds_engine.module(**tokenized_inputs)
+                    logits = outputs["logits"].detach().to(device="cpu", dtype=torch.float32)
 
-            # need to remove batch dimension
-            all_logits.append(torch.squeeze(logits))
-            all_tokens.append(torch.squeeze(tokenized_inputs["input_ids"]))
-        return all_logits, all_tokens
+                    # need to remove batch dimension
+                    all_logits.append(torch.squeeze(logits))
+                    all_tokens.append(torch.squeeze(tokenized_inputs["input_ids"]))
+            except StopIteration:  # TODO: should be iterator empty error
+                # print("exhausted")
+                return {"all_logits": all_logits, "all_tokens": all_tokens}
 
-    def evaluate_classification(self, examples):
-        prompts = [
-            example["prompt"] + class_seq
-            for example in examples
-            for class_seq in example["classes"]
-        ]
-
-        all_logits, all_tokens = self.get_logits_and_tokens(prompts)
-
-        # for each possible class sequence, we need to get the logprob on the full class sequence
-        n_classes = len(examples[0]["classes"])
-        total_logprobs = []
-        losses = []
-        labels_correct = []
-
-        for i, example in enumerate(examples):
-            prompt_start = i * n_classes
-            class_logprobs = []
-            for j in range(n_classes):
-                class_index = prompt_start + j
-                class_logits = all_logits[class_index]
-                # the lengths of each class sequence in tokens
-                class_sequence = example["classes"][j]
-                target_token_length = len(self.tokenizer(class_sequence)["input_ids"])
-                # we only need the logits for the end sequence
-                tokens = all_tokens[class_index]
-                # we have to go back by one because we don't care about the logits for the predicted token
-                sequence_logits = class_logits[-target_token_length - 1: -1]
-                sequence_tokens = tokens[-target_token_length:]
-                # we take a log_softmax over all token logits for each position in the class sequence to
-                #  get log probabilities, and then sum the logprobs for the tokens actually chosen
-                logprobs = F.log_softmax(sequence_logits, dim=-1)
-                class_logprob = sum(
-                    [logprobs[i, token] for i, token in enumerate(sequence_tokens)]
-                )
-                class_logprobs.append(class_logprob.item())  # the sum is never empty so never just 0, always a tensor
-
-            total_logprob = sum(class_logprobs)
-            normalised_logprobs = F.log_softmax(torch.tensor(class_logprobs), dim=-1)
-            loss = -normalised_logprobs[example["answer_index"]].item()
-            label_correct = int(np.argmax(normalised_logprobs) == example["answer_index"])
-            total_logprobs.append(total_logprob)
-            losses.append(loss)
-            labels_correct.append(label_correct)
-        return {
-            "loss": losses,
-            "correct": labels_correct,
-            "total_logprob": total_logprobs,
-            "accuracy": sum(labels_correct) / len(labels_correct),
-            "mean_loss": sum(losses) / len(losses)
-        }
+         # TODO: run evaluation here after inference
 
 
 if __name__ == '__main__':
 
-    NUM_TRUNCATE = 5
+    NUM_TRUNCATE = 16
 
-    infer = Inference()
-    dataset = infer.load_dataset(num_truncate=NUM_TRUNCATE)
+    ds = load_dataset(num_truncate=NUM_TRUNCATE)
+    ds_distributed = prepare(ds, batch_size=8)  # this is per-accelerator batch size
+    ds_distributed_iter = iter(ds_distributed)
 
     st = time.time()
-    result = infer.evaluate_classification(dataset)
+    infer = Inference()
+    result = infer.evaluate_classification(ds_distributed_iter)
+
     print(f"Time taken: {time.time() - st}")
 
-    print(result)
+    print(result['all_logits'][0])
+    print(result['all_logits'][0].size())
+    print(result['all_logits'][1])
+    print(f"are 0 and 1 the same? {result['all_logits'][0] == result['all_logits'][1]}")
+    # print(result)
 
 
 test_examples = [
